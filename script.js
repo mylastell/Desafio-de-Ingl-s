fb067073618c36c1522ebf5f081f05396b471df4
unknown <mylastell>
2022-07-11 01:39:19 -0300

    finish

---
+++ script.js
@@ -0,0 +1,47 @@
+//Crie 3 variáveis, cada uma com um array:
+
+let filme = ["Comedy", "Action", "Fantasy"]
+console.log("Categorias de filme",filme)
+
+//Dentro de cada array, insira 3 (TRÊS) Objetos, contendo:
+
+let Comedy = [
+    {
+Tilte: "Os farofeiros",
+Director: "Roberto Santucci",
+Year: 2018
+},
+
+]
+
+console.log(Comedy)
+
+let Action = [
+    {
+Tilte: "Tropa de Elite",
+Director: "José Padilha",
+Year: 2007
+
+},
+]
+
+console.log(Action)
+
+let Fantasy = [
+    {
+ Tilte: "A fantastica fabrica de chocolate",
+ Director: "Tim Burton",
+ Year: 2005
+    }
+]
+//DESAFIO EXTRA 
+
+//Escolha uma variável e insira uma condição para ela, (condição de sua escolha), com a seguinte mensagem ao console: 
+
+let categoria = "Tropa de Elite"
+
+if(categoria[0] === "T"){
+console.log("Esse filme é dez")
+}else{
+ console.log("Tem melhor!")
+}
